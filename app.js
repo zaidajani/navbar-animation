fa6e9c6ba603a376ae54693fd67514501e592108
Zaid <zaidajani2007@gmail.com>
2020-09-06 22:39:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger');
+const links = document.querySelector('.links');
+
+burger.addEventListener('click', ()=> {
+    links.classList.toggle('open');
+})
